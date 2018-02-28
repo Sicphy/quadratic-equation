8a15c228c7d2c218afd6ac4d8fdc24b965994f42
Sicphy <petrov.kirill.1998@mail.ru>
2018-02-28 23:10:39 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 module.exports = function solveEquation(equation) {
   // your implementation
+    var ur = equation;
+    var temp = "";
+    var a,b,c,d,x1,x2;
+    var arr = [];
+
+    for(var i = 0; i<ur.length; i++) {
+        temp += ur[i];
+        if(ur[i] === ' ' || i == ur.length-1) {
+            if(temp.indexOf('x') != -1) {
+                if(temp.indexOf('^') != -1)
+                    a = 1;
+                else
+                    b = 1;
+                temp = "";
+                continue;
+            }
+
+            if(ur[i+1] === '+' || ur[i+1] === '-' || i == ur.length-1) {
+                c = eval(temp);
+                temp = '';
+            }
+
+            if(ur[i+1] === '*')
+                if(ur[i+4] === '^') {
+                    i+=5;
+                    a = eval(temp);
+                    temp = '';
+                }
+                else {
+                    i+=3;
+                    b = eval(temp);
+                    temp = '';
+                }
+        }
+    }
+
+    if( c === undefined )
+        c = 0;
+    if( a === undefined)
+        a = 0;
+    if(b === undefined)
+        b = 0;
+
+    d = b*b - 4*a*c;
+
+    if(d<0)
+        throw new Error("d < 0");
+
+    x1 = (-b + Math.sqrt(d))/(2*a);
+    x2 = (-b - Math.sqrt(d))/(2*a);
+
+    if(x1 < 0)
+    x1 = parseInt(x1 - 0.5);
+else
+    x1 = parseInt(x1 + 0.5);
+
+    if(x2 < 0)
+        x2 = parseInt(x2 - 0.5);
+    else
+        x2 = parseInt(x2 + 0.5);
+
+    arr.push(parseInt(x1));
+    arr.push(parseInt(x2));
+
+    return arr;
 }
